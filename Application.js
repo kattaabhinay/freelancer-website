d96c34ba487a530fb79234327e664a4355e13e75
kattaabhinay <kattaabhinay82@gmail.com>
2026-04-08 14:56:18 +0530

    Delete Application.js

--- Application.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const applicationSchema = new mongoose.Schema({
-    jobId: String,
-    email: String
-});
-
-module.exports = mongoose.model('Application', applicationSchema);
