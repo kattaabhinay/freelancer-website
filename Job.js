91835a12cffb7b55b5040bf356cd292630c25a62
kattaabhinay <kattaabhinay82@gmail.com>
2026-04-08 14:56:36 +0530

    Delete Job.js

--- Job.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const jobSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    budget: Number
-});
-
-module.exports = mongoose.model('Job', jobSchema);
